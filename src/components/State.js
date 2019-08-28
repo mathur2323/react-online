275ec76d9e18740b6579dc160ed18f3efb84590a
Rohit <nathur2323@gmail.com>
2019-08-28 23:42:55 +0530

    Initital Commit

---
+++ src/components/State.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+class State extends Component {
+    constructor(){
+        super();
+        this.state = {
+          name:'Stateful Component'  
+        }
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h1>A {this.state.name} with state</h1>
+                <button onClick="abc()">Update Name</button>
+            </div>
+        )
+    }
+}
+
+export default State
